9ffa4b1aa95f8b135ce7ceff7f91d8f0cbefb5f7
Lorenzo <107998561+lorenzoworx@users.noreply.github.com>
2023-05-31 15:04:30 +0100

    DEFINE HANDLECLICK FUNCTION FOR HANDLING CALCULATOR OPERATIONS

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -1,11 +1,37 @@
+import { useState } from 'react';
+import calculate from '../logic/calculate';
 import CalcBuild from './CalcBuild';
 import CalcDisplay from './CalcDisplay';
 
-const Calculator = () => (
-  <div className="calculator">
-    <CalcDisplay />
-    <CalcBuild />
-  </div>
-);
+const calcObject = {
+  total: null,
+  next: null,
+  operation: null,
+};
+
+const Calculator = () => {
+  const [calcResult, setCalcResult] = useState('0');
+  const handleClick = (e) => {
+    const {
+      total = null,
+      next = null,
+      operation = null,
+    } = calculate(calcObject, e.target.value);
+    calcObject.total = total;
+    calcObject.next = next;
+    calcObject.operation = operation;
+    if (calcObject.total || calcObject.next) {
+      setCalcResult(calcObject.next ? calcObject.next : calcObject.total);
+    } else {
+      setCalcResult('0');
+    }
+  };
+  return (
+    <div className="calculator">
+      <CalcDisplay result={calcResult} />
+      <CalcBuild handleClick={handleClick} />
+    </div>
+  );
+};
 
 export default Calculator;
