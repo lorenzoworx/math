f140b410a042acec88ce9efdd1bc61e9ee7d0871
Lorenzo <107998561+lorenzoworx@users.noreply.github.com>
2023-05-31 15:01:45 +0100

    DISPLAY CALCULATOR DISPLAY IN DISPLAY COMPONENT

--- src/components/CalcDisplay.jsx
+++ src/components/CalcDisplay.jsx
@@ -1,7 +1,13 @@
-const CalcDisplay = () => (
+import PropTypes from 'prop-types';
+
+const CalcDisplay = ({ result }) => (
   <div className="calculatorDisplay">
-    <span className="current">0</span>
+    <span className="current">{result}</span>
   </div>
 );
+
+CalcDisplay.propTypes = {
+  result: PropTypes.string.isRequired,
+};
 
 export default CalcDisplay;
