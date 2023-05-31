474454e16b72f2001a83d4727a75c44ede3b0c3b
Lorenzo <107998561+lorenzoworx@users.noreply.github.com>
2023-05-31 15:00:01 +0100

    ADD ONCLICK ATTRIBUTE TO BUTTONS

--- src/components/CalcBuild.jsx
+++ src/components/CalcBuild.jsx
@@ -1,15 +1,22 @@
-const CalcBuild = () => {
-  const features = ['AC', '+/-', '%', '÷', '7', '8', '9', 'x', '4', '5', '6', '-',
-    '1', '2', '3', '+', '0', '.', '='];
+import PropTypes from 'prop-types';
+
+const CalcBuild = ({ handleClick }) => {
+  const features = ['AC', '+/-', '%', '÷', '7', '8',
+    '9', 'x', '4', '5', '6', '-', '1', '2', '3', '+',
+    '0', '.', '='];
   return (
     <div className="calc">
       {features.map((feature) => (
-        <button type="button" key={feature}>
+        <button type="button" key={feature} value={feature} onClick={handleClick}>
           {feature}
         </button>
       ))}
     </div>
   );
+};
+
+CalcBuild.propTypes = {
+  handleClick: PropTypes.func.isRequired,
 };
 
 export default CalcBuild;
